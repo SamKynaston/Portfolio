e75dbda89e66dbaf12c43d44643c3e43c00e56a5
samkynaston <samkynno@icloud.com>
2026-08-17 02:20:13 +0100

    added a doodle canvas

---
+++ src/components/canvas.tsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from 'react';
+
+const Canvas: React.FC = (props) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [drawing, setDrawing] = useState(false);
+
+    const getPosition = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if (!canvas) return { x: 0, y: 0 };
+        const rect = canvas.getBoundingClientRect();
+        return {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+        };
+    }
+
+    const onMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext('2d');
+
+        if (!canvas || !context) return;
+
+        const { x, y } = getPosition(event);
+
+        context.beginPath();
+        context.moveTo(x, y);
+
+        setDrawing(true);
+    };
+
+    const onMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        if (!drawing) return;
+
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext('2d');
+
+        if (!canvas || !context) return;
+
+        const { x, y } = getPosition(event);
+
+        context.lineWidth = 3;
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+
+        context.lineTo(x, y);
+        context.stroke();
+    };
+
+    const onMouseUp = () => {
+        setDrawing(false);
+    };
+
+    return <canvas ref={canvasRef} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} onMouseLeave={onMouseUp} {...props} />;
+}
+
+export default Canvas;
